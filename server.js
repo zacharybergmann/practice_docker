4b3b7c53ffba3ce2c7e4c5255fed98881f6e9d3c
Zachary Bergmann <Zachary Bergmann>
2017-05-23 21:43:45 -0500

    (prog) Test for docker deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+const PORT = 3000;
+const pg = require('pg');
+pg.connect('postgres://postgres:password@localhost:5432/practicedocker');
+
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+server.get('/', (req, res) => res.status(200).send('hello'));
+
